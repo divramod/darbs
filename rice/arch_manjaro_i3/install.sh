ea668ca98f41ff387bb6376d4f7cd4bc38579565
divramod <divramod@gmail.com>
2019-08-16 17:49:13 +0200

    add goinstall, npminstall, pipinstall

--- rice/arch_manjaro_i3/install.sh
+++ rice/arch_manjaro_i3/install.sh
@@ -94,14 +94,27 @@ gitmakeinstall() {
 aurinstall() { \
 	dialog --title "LARBS Installation" --infobox "Installing \`$1\` ($n of $total) from the AUR. $1 $2" 5 70
 	echo "$aurinstalled" | grep "^$1$" >/dev/null 2>&1 && return
-	sudo -u "$name" $aurhelper -S --noconfirm "$1" >/dev/null 2>&1
-	}
+	sudo -u "$name" $aurhelper -S --noconfirm "$1" >/dev/null 2>&1 ;}
 
 pipinstall() { \
 	dialog --title "LARBS Installation" --infobox "Installing the Python package \`$1\` ($n of $total). $1 $2" 5 70
 	command -v pip || pacman -S --noconfirm --needed python-pip >/dev/null 2>&1
-	yes | pip install "$1"
-	}
+	yes | pip install "$1" --user ;}
+
+npminstall() { \
+	dialog --title "LARBS Installation" --infobox "Installing the npm-package \`$1\` ($n of $total). $1 $2" 5 70
+  command -v npm || pacman -S --noconfirm --needed npm >/dev/null 2>&1
+  npm install -g "${1}" ;}
+
+goinstall() { \
+	dialog --title "LARBS Installation" --infobox "Installing the npm-package \`$1\` ($n of $total). $1 $2" 5 70
+  command -v go || pacman -S --noconfirm --needed go >/dev/null 2>&1
+  export GO111MODULE=on
+	dir=$(mktemp -d)
+	git clone --depth 1 "$1" "$dir" >/dev/null 2>&1
+	cd "$dir" || exit
+  go install
+	cd /tmp || return ;}
 
 installationloop() { \
 	([ -f "$progsfile" ] && cp "$progsfile" /tmp/progs.csv) || curl -Ls "$progsfile" | sed '/^#/d' > /tmp/progs.csv
@@ -115,6 +128,8 @@ installationloop() { \
 			"A") aurinstall "$program" "$comment" ;;
 			"G") gitmakeinstall "$program" "$comment" ;;
 			"P") pipinstall "$program" "$comment" ;;
+			"N") npminstall "$program" "$comment" ;;
+			"O") goinstall "$program" "$comment" ;;
 		esac
 	done < /tmp/progs.csv ;}
 

1409878e82b05ad59742016037c991a956c592a5
divramod <divramod@gmail.com>
2019-08-16 19:07:25 +0200

    add goinstall, npminstall, pipinstall

--- rice/arch_manjaro_i3/install.sh
+++ rice/arch_manjaro_i3/install.sh
@@ -103,8 +103,8 @@ pipinstall() { \
 
 npminstall() { \
 	dialog --title "LARBS Installation" --infobox "Installing the npm-package \`$1\` ($n of $total). $1 $2" 5 70
-  command -v npm || pacman -S --noconfirm --needed nodejs >/dev/null 2>&1
-  npm install -g "${1}" ;}
+  command -v npm || pacman -S --noconfirm --needed npm >/dev/null 2>&1
+  sudo npm install -g "${1}" ;}
 
 goinstall() { \
 	dialog --title "LARBS Installation" --infobox "Installing the npm-package \`$1\` ($n of $total). $1 $2" 5 70
@@ -113,7 +113,7 @@ goinstall() { \
 	dir=$(mktemp -d)
 	git clone --depth 1 "$1" "$dir" >/dev/null 2>&1
 	cd "$dir" || exit
-  go install
+  go install >/dev/null 2>&1
 	cd /tmp || return ;}
 
 installationloop() { \

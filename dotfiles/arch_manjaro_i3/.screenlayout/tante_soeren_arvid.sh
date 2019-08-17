fe833e24a69f297de469a728cab913983eecbcd6
divramod <divramod@gmail.com>
2019-08-17 07:31:03 +0200

    update

--- dotfiles/arch_manjaro_i3/.screenlayout/tante_soeren_arvid.sh
+++ dotfiles/arch_manjaro_i3/.screenlayout/tante_soeren_arvid.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output DP3 --off --output eDP1 --mode 1600x900 --pos 0x560 --rotate normal --output DP1 --off --output HDMI3 --off --output HDMI2 --off --output HDMI1 --primary --mode 1920x1200 --pos 1600x0 --rotate normal --output DP2 --off
+xrandr --output eDP1 --mode 2560x1440 --pos 0x1200 --rotate normal --output DP1 --off --output DP2 --off --output DP3 --off --output HDMI1 --primary --mode 1920x1200 --pos 320x0 --rotate normal --output HDMI2 --off --output HDMI3 --off --output VIRTUAL1 --off
